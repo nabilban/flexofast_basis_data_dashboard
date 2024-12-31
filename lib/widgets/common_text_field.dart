e2dd3f380679ec9214d606fbf73c56065f807cd0
nabilban <nabilban354@gmail.com>
2024-12-31 23:27:48 +0700

    common text field

---
+++ lib/widgets/common_text_field.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CommonTextField extends StatelessWidget {
+  const CommonTextField({
+    super.key,
+    required this.controller,
+    required this.label,
+  });
+
+  final TextEditingController controller;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label,
+        border: const OutlineInputBorder(),
+      ),
+    );
+  }
+}

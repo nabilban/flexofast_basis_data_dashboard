7902b17686951e28716a77a71cda2f8e836ffa3b
nabilban <nabilban354@gmail.com>
2025-01-02 17:26:03 +0700

    list sewa

---
+++ lib/features/sewa/pages/list_sewa_page.dart
@@ -0,0 +1,71 @@
+import 'package:flexofast_basis_data_dashboard/features/sewa/pages/penyewaan_page.dart';
+import 'package:flexofast_basis_data_dashboard/navigate.dart';
+import 'package:flexofast_basis_data_dashboard/utils.dart';
+import 'package:flexofast_basis_data_dashboard/widgets/common_scaffold.dart';
+import 'package:flexofast_basis_data_dashboard/widgets/loading_widget_state.dart';
+import 'package:flexofast_basis_data_dashboard/widgets/empty_widget_state.dart';
+import 'package:flexofast_basis_data_dashboard/features/sewa/cubit/sewa_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ListSewaPage extends StatelessWidget {
+  const ListSewaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider.value(
+      value: SewaCubit()..getAllSewa(),
+      child: CommonScaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigate.push(context, const PenyewaanPage());
+          },
+          child: const Icon(Icons.addchart),
+        ),
+        title: 'List Sewa',
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: BlocBuilder<SewaCubit, SewaState>(
+            builder: (context, state) {
+              return state.maybeWhen(
+                orElse: () => const LoadingWidgetState(),
+                loading: () => const LoadingWidgetState(),
+                loaded: (list) {
+                  if (list.isEmpty) {
+                    return const EmptyWidgetState();
+                  }
+                  return ListView.builder(
+                    itemCount: list.length,
+                    itemBuilder: (context, index) {
+                      final sewa = list[index];
+                      return Card(
+                        margin: const EdgeInsets.symmetric(vertical: 4),
+                        child: ListTile(
+                          onTap: () {},
+                          title: Text(
+                            'Sewa ${sewa.id}',
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Client: ${sewa.idClient}'),
+                              Text('Gudang: ${sewa.idGudang}'),
+                              Text(
+                                'Tanggal: ${Utils.formatDate(sewa.tanggalMulai)} - ${Utils.formatDate(sewa.tanggalAkhir)}',
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}

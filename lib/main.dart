8a639a5ba0a699eb6e820e9f318187f6a1d5a0cf
Tamma <dev.mufti@gmail.com>
2025-01-02 19:01:07 +0700

    print doc location

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'dart:io';
+
 import 'package:flexofast_basis_data_dashboard/provider.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory appDir = await getApplicationDocumentsDirectory();
+  print(appDir.path);
+
   runApp(const GlobalProvider());
 }
